3b8299f4c873b7c7375f6ff3a680e350fb21a599
Anucha Prapphrom <anucha.21681@hotmail.com>
2020-03-31 11:52:35 +0700

    feat(system): Update config

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ const Home = () => (
   <div>
     <Head>
       <title>Home</title>
-      <link rel="icon" href="/static/favicon.ico" importance="low" />
+    
     </Head>
 
     <div className="hero">
